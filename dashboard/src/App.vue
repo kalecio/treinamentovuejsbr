52b52058b05c050978d9fa256778b6110a38f616
kalecio <kalecio.santana@gmail.com>
2021-02-01 19:39:26 -0300

    feat: defined ModalFactory component

--- dashboard/src/App.vue
+++ dashboard/src/App.vue
@@ -1,3 +1,12 @@
 <template>
-  <router-view/>
+  <modal-factory />
+  <router-view />
 </template>
+
+<script>
+import ModalFactory from './components/ModalFactory'
+
+export default {
+  components: { ModalFactory }
+}
+</script>
