eb2d12c8a0e7edaf0109fb9946c189c664527d62
kalecio <kalecio.santana@gmail.com>
2021-02-01 19:42:19 -0300

    feat: implement ModalFactory

--- dashboard/src/components/ModalFactory/index.vue
+++ dashboard/src/components/ModalFactory/index.vue
@@ -6,25 +6,37 @@
       @click="handleModalToggle({ status: false })"
     >
       <div
-        class="fixed mx-10 "
+        class="fixed mx-10"
+        :class="state.width"
+        @click.stop
       >
         <div class="flex flex-col overflow-hidden bg-white rounded-lg animate__animated animate__fadeInDown animate__faster">
-          <div class="flex flex-col px-12 py-10 bg-white ">
+          <div class="flex flex-col px-12 py-10 bg-white">
             <component :is="state.component" />
           </div>
         </div>
 
       </div>
     </div>
-  </teleport>  
+  </teleport>
 </template>
 
 <script>
-import { reactive } from 'vue'
+import { reactive, onMounted, onBeforeUnmount, defineAsyncComponent } from 'vue'
+import useModal from '../../hooks/useModal'
+
+const ModalLogin = defineAsyncComponent(() => import('../ModalLogin'))
+const ModalCreateAccount = defineAsyncComponent(() => import('../ModalCreateAccount'))
+
+const modal = useModal()
 
 const DEFAULT_WIDTH = 'w-3/4 lg:w-1/3'
 
 export default {
+  components: {
+    ModalLogin,
+    ModalCreateAccount
+  },
   setup () {
     const state = reactive({
       isActive: false,
@@ -33,11 +45,31 @@ export default {
       width: DEFAULT_WIDTH
     })
 
-    function handleModalToggle ({ status }) {
+    onMounted(() => {
+      modal.listen(handleModalToggle)
+    })
+
+    onBeforeUnmount(() => {
+      modal.off(handleModalToggle)
+    })
+
+    function handleModalToggle (payload) {
+      if (payload.status) {
+        state.component = payload.component
+        state.props = payload.props
+        state.width = payload.width ?? DEFAULT_WIDTH
+      } else {
+        state.component = {}
+        state.props = {}
+        state.width = DEFAULT_WIDTH
+      }
+
+      state.isActive = payload.status
     }
 
     return {
-      state
+      state,
+      handleModalToggle
     }
   }
 }
